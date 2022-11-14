35aa4b2267f05a5944aae8a74ea80b47c4521904
Dmitrii Sobin <neo7976@yandex.ru>
2022-11-14 09:11:58 +0300

    Post save and getAll

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -2,24 +2,41 @@ package ru.netology.repository;
 
 import ru.netology.model.Post;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 // Stub
 public class PostRepository {
-  public List<Post> all() {
-    return Collections.emptyList();
-  }
+    private List<Post> list = new ArrayList<>();
+    private final AtomicLong aLong = new AtomicLong(0);
 
-  public Optional<Post> getById(long id) {
-    return Optional.empty();
-  }
+    public List<Post> all() {
+        if (!list.isEmpty())
+            return list;
+        return Collections.emptyList();
+    }
 
-  public Post save(Post post) {
-    return post;
-  }
+    public Optional<Post> getById(long id) {
 
-  public void removeById(long id) {
-  }
+        return Optional.empty();
+    }
+
+    public synchronized Post save(Post post) {
+        //todo если id поста нет, то сохранить как следующий или выдать ошибку
+        if (post.getId() == 0 || !list.contains(post)) {
+            post.setId(aLong.incrementAndGet());
+            list.add(post);
+        }
+        int x = list.indexOf(post);
+        list.set(x, post);
+        return post;
+    }
+
+
+    public void removeById(long id) {
+    }
 }
